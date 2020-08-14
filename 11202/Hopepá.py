fd10ce24d46d4cd02ef7da0a64eba5e91322b8ff
lep987 <1961thelma>
2020-08-14 20:32:38 -0300

    HOPA

--- 11202/Hopepá.py
+++ 11202/Hopepá.py
@@ -1,8 +1,11 @@
 class jove:
-    def satu(self):
+    def __init__(self, nome = None, idade = None):
+        self.nome = nome
+        self.idade = idade
+    def jup(self):
         return 'Olá'
 
 if __name__ == '__main__':
-    p = jove()
-    print (p.satu())
-    print (p.satu())
+    p = jove('Julio', 35)
+    print (p.jup())
+    print (p.jup(),',', p.nome,'Tu tens', p.idade, 'anos, né?')
